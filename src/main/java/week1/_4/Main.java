55c3bdec39983d194c762d055fd528e5fbba89be
Rohan Chandra <76585654+DrXfqqt@users.noreply.github.com>
2021-08-10 22:57:04 +0530

    Update Main.java

--- src/main/java/week1/_4/Main.java
+++ src/main/java/week1/_4/Main.java
@@ -1,12 +1,16 @@
-package week1._4;
-
-public class Main {
+public class Prgm {
     public static void main(String[] args) {
-        System.out.println("Value of 10&50 is: " + (10&50));
-        System.out.println("Value of 30|40 is: " + (30|40));
-        System.out.println("Value of ~50 is: " + (~50));
-        System.out.println("Value of 60>>3 is: " + (60>>3));
-        System.out.println("Value of 70<<8 is: " + (70<<8));
-        System.out.println("Value of 55>>>2 is: " + (55>>>2));
+        System.out.print("10&50 = ");
+        System.out.println(10&50);
+        System.out.print("30||40 = ");
+        System.out.println(30|40);
+        System.out.print("~50 = ");
+        System.out.println(~50);
+        System.out.print("60>>3 = ");
+        System.out.println(60>>3);
+        System.out.print("70<<8 = ");
+        System.out.println(70<<8);
+        System.out.print("55>>>2 = ");
+        System.out.println(55>>>2);
     }
 }
