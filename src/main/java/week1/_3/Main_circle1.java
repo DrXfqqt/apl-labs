d8791dc9117fc5bc63c1b4052acdcbbda7461b10
Rohan Chandra <76585654+DrXfqqt@users.noreply.github.com>
2021-08-12 19:13:20 +0530

    Delete src/main/java/week1/_3 directory

--- src/main/java/week1/_3/Main_circle1.java
+++
@@ -1,8 +0,0 @@
-package week1._3;
-
-public class Main_circle1 {
-    public static void main(String[] args) {
-        Circle1.calcArea(4);
-        Circle1.calcArea();
-    }
-}
--- src/main/java/week1/_3/Main_ractangle1.java
+++
@@ -1,9 +0,0 @@
-package week1._3;
-
-public class Main_ractangle1 {
-    public static void main(String[] args) {
-        Rectangle1.calcArea(2, 3);
-        Rectangle1.calcArea(5);
-        Rectangle1.calcArea();
-    }
-}
