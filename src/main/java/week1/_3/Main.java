b2948080d4359bd2568322c4af32c0d993e90a6a
Rohan Chandra <76585654+DrXfqqt@users.noreply.github.com>
2021-08-12 23:09:15 +0530

    Update Main.java

--- src/main/java/week1/_3/Main.java
+++ src/main/java/week1/_3/Main.java
@@ -1,70 +1,51 @@
 import java.util.Scanner;
-class Cuboid{
-    int length, width, height;
-    void set_length(int l){
+class Rectangle{
+    int length;
+    int width;
+    Rectangle(int l, int w){
         length = l;
+        width = w; 
 }
-void set_width(int w){
-width = w; }
-    void set_height(int h){
-        height = h;
-}
-void volume(){
-System.out.println("The volume of given cuboid is: " + (length*width*height));
-} }
-class circle{
-    int radius;
-    void set_radius(int r){
-        radius = r;
-}
-
 void area(){
-        System.out.println("The Area of given circle is: " +
-        3.14*radius*radius); }
-        }
-        class rectangle{
+        System.out.println("The area of given rectangle: " + length*width);
+        } }
+        class Cuboid{
             int length;
-        int width;
-            void set_rlength(int l){
+        int width; int height;
+            Cuboid(int l, int w, int h){
                 length = l;
+        width = w;
+        height = h; }
+        void volume(){
+        System.out.println("The volume of given cuboid is: " +
+        length*width*height); }
         }
-        void set_rwidth(int w){
-        width = w; }
-        void rarea(){
-        System.out.println("The area of the given rectangle is: " +
-        length*width); }
-        }
-public class Main{
-        public static void main(String args[]){
-        
-        Cuboid c1 = new Cuboid();
-        System.out.println("Enter the length of cuboid: ");
-        Scanner sc = new Scanner(System.in);
-        int l = sc.nextInt();
-        c1.set_length(l);
-        System.out.println("Enter the width of cuboid: ");
-        int w = sc.nextInt();
-        c1.set_width(w);
-        System.out.println("Enter the height of the cuboid: ");
-        int h = sc.nextInt();
-        c1.set_height(h);
-
-        c1.volume();
-
-        circle c2 = new circle();
-         System.out.println("Enter int radius of the cicle: ");
-        int r = sc.nextInt(); 
-        c2.set_radius(r); 
-        c2.area();
-       
-        rectangle r1 = new rectangle();
-        System.out.println("Enter the length of rectangle: "); 
-        int lr = sc.nextInt();
-        r1.set_rlength(lr);
-        System.out.println("Enter the width of rectangle: ");
-        int wr = sc.nextInt();
-        r1.set_rwidth(wr);
-        r1.rarea();
-
+class Circle{
+                int radius;
+                Circle(int r){ radius = r;
+                }
+                void area(){
+                System.out.println("The area of given circle is: " + 3.14*radius*radius);
+                } }
+                public class Main{
+                    public static void main(String args[]){
+                Scanner sc = new Scanner(System.in);
+                for(int i = 0; i < 3; i++) {
+                System.out.println("Enter the length of cuboid: ");
+                int l = sc.nextInt();
+                System.out.println("Enter the width of cuboid: ");
+                int w = sc.nextInt();
+                System.out.println("Enter the height of the cuboid: "); int h = sc.nextInt();
+                Cuboid c1 = new Cuboid(l, w, h);
+                c1.volume(); }
+                System.out.println("Enter the radius of the circle: "); int r = sc.nextInt();
+                Circle c2 = new Circle(r);
+                c2.area();
+                System.out.println("Enter the length of rectangle: ");
+                        int lr = sc.nextInt();
+                System.out.println("Enter the width of rectangle: "); 
+                        int wr = sc.nextInt();
+                        Rectangle r1 = new Rectangle(lr, wr);
+                r1.area(); 
         }
 }
